60f85acee236d83df7bed7b37ecf12a20ad9a0a7
A.MOHAMMED YASEEN (192121129) <112368880+yaseen1234567890@users.noreply.github.com>
2023-11-22 20:07:31 +0530

    Add files via upload

---
+++ 6th exp.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture("C:/Users/Mohammed Yaseen A/Downloads/6.jpg")
+if(cap.isOpened()== False):
+print("Error opening video file")
+while(cap.isOpened()):
+ret, frame = cap.read()
+if ret == True:
+cv2.imshow('Frame', frame)
+if cv2.waitKey(250) & 0xFF == ord('q'):
+break
+else:
+break
+cap.release()
+cv2.destroyAllWindows()
